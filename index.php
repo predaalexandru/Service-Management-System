bb42301dd010fb170881f775ddd697c2595b0152
Preda Alexandru <alexpreda95@yahoo.com>
2020-10-31 12:04:06 +0200

    Creating the navbar and start header

--- index.php
+++ index.php
@@ -1,1 +1,77 @@
-<?php echo 'Hello TERMO DINAMIC'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="css/all.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+
+    <title>TERMO DINAMIC</title>
+</head>
+
+<body>
+    <!-- Start Navigation -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
+        <a href="index.php" class="navbar-brand">Termo Dinamic</a>
+        <span class="navbar-text">Quality Works</span>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="myMenu">
+            <ul class="navbar-nav pl-5 custom-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Services">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Registration">Registration</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- End Navigation -->
+
+    <!-- Start Header Jumbotron -->
+    <header class="jumbotron back-image" style="background-image: url(img/banner2.jpg);">
+        <div>
+            <h1 class="text-uppercase text-dark font-weight-bold">Welcome to TERMO DINAMIC</h1>
+            <p class="font-italic">Quality Works</p>
+            <a href="#" class="btn btn-success mr-4">Login</a>
+            <a href="#" class="btn btn-danger mr-4">Sign Up</a>
+        </div>
+    </header>
+    <!-- End Header Jumbotron -->
+
+
+    <!-- Jquery -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="js/popper.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome JS -->
+    <script src="js/all.js"></script>
+</body>
+</html>
